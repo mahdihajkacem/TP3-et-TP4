4f99c322cdedb9282ac834e188d4ac28551bd0a7
Mahdi Hajkacem <hajkacemmahdi@gmail.com>
2022-06-19 16:22:36 +0100

    TP3 et TP4 Asp .Net

---
+++ Migrations/20220601205433_MajIdentity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionEtudiant.Migrations
+{
+    public partial class MajIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Models/StudentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionEtudiant.Models.Repositories
+{
+    public class StudentContext : IdentityDbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<School> Schools { get; set; }
+
+    }
+}
